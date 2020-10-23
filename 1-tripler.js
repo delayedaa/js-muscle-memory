02152071103ca6181ada86a0d94e600677dd8469
delayedaa <delayedaa@gmail.com>
2020-10-23 14:36:10 -0500

    [1-tripler.js] Complete tripler function

--- 1-tripler.js
+++ 1-tripler.js
@@ -9,5 +9,27 @@ tripler([4, 1, 7]); // => [ 12, 3, 21 ]
 *******************************************************************************/
 
 function tripler(array) {
-  
+  const result = [];
+  console.log('Inside of tripler function');
+  for (let i = 0; i < array.length; i++) {
+    let num = array[i];
+    let multiple = num * 3;
+    result.push(multiple);
+  }
+  return result;
 }
+
+console.log(tripler([1, 2, 3]));
+console.log(tripler([4, 1, 7]));
+
+
+// What am I returning? An array.
+
+// Return a new array.
+// Iterate through the array passed in.
+// Multiply each element by 3.
+// Push the solution into my result.
+// Return the result.
+
+
+// return array.map(element => element * 3)
